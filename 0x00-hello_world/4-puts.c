2972c742d3e8dd5f976db05ccce871d0817936db
Robert Mawusi Kpodo <mawusikpodo@yahoo.com>
2023-02-16 07:30:08 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - The main function
+ * Return: a string
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
