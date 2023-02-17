356c3fefbb99fed18b96e2451ebfb4fc8cfdd61c
Robert Mawusi Kpodo <mawusikpodo@yahoo.com>
2023-02-17 12:28:29 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - loop through decimal numbers from 0 to 9
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10;  i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
