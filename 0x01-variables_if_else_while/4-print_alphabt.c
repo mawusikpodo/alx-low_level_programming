cb77b6b6eedc9d874b7bb36a294dec75caec654e
Robert Mawusi Kpodo <mawusikpodo@yahoo.com>
2023-02-17 12:03:50 -0800

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+			putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
