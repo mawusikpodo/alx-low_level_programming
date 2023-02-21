629c38f627a6ca75224f62a7358d3c3a7de46e41
Robert Mawusi Kpodo <mawusikpodo@yahoo.com>
2023-02-21 05:32:45 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry Point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char word[9] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+		_putchar(word[i]);
+	_putchar('\n');
+	return (0);
+}
