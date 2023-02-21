686bd42ac1b5a10476773a81d5f445815da8fa40
Robert Mawusi Kpodo <mawusikpodo@yahoo.com>
2023-02-21 15:30:12 -0800

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Function
+ * @r: input parameter
+ * Description: find absolute value
+ * Return: absolute value
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
