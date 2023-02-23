6086a63477b83a815163d9dc46ec137b1ad27757
Robert Mawusi Kpodo <mawusikpodo@yahoo.com>
2023-02-23 13:15:49 -0800

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,11 +9,11 @@
 void print_line(int n)
 {
 	int i;
-	for (i = 0; i <= n; i++)
+	for (i = 1; i <= n; i++)
 	{
 		if (n > 0)
 			_putchar('_');
-		if (n <= 0)
+		else
 			_putchar('\n');
 	}
 	_putchar('\n');
