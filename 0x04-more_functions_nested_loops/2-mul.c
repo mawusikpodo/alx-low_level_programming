5e73b46cc5f578d41d6e4fe1c57da9f33cb1ad19
Robert Mawusi Kpodo <mawusikpodo@yahoo.com>
2023-02-23 08:41:28 -0800

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - check description
+ * @a: first input
+ * @b: second input
+ * Description: calculate the product of two numbers
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
