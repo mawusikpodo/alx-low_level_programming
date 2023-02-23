92669dc8a8081b1f7d731ef51d35827e9e1102d5
Robert Mawusi Kpodo <mawusikpodo@yahoo.com>
2023-02-23 09:51:05 -0800

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - check description
+ * @n: input charactor
+ * Description: prints out a straight line
+ */
+
+void print_line(int n)
+{
+	int i;
+	for (i = 0; i <= n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		_putchar('_');
+	}
+	_putchar('\n');
+}
