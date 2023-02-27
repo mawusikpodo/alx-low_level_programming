dbef449971f9a55691f2da450da92922eef072a9
Robert Mawusi Kpodo <mawusikpodo@yahoo.com>
2023-02-27 07:21:50 -0800

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Check description
+ * @n: Input parameter
+ * Description: The function takes a pointer as
+ * parameter and updates the value it points to to 98
+ * Return: Always 0;
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
