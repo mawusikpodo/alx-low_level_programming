39ddfc0c5f40d8332619fbfbabb40adb13e5994b
Robert Mawusi Kpodo <mawusikpodo@yahoo.com>
2023-03-20 14:44:34 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Structure for a dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ * Description: Data structure for a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
