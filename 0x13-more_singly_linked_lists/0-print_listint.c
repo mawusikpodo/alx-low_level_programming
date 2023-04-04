72b5a13e32920a575c8eac9a9476d351823b2bd0
Robert Mawusi Kpodo <mawusikpodo@yahoo.com>
2023-04-04 04:58:52 -0700

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,15 +10,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t number_of_nodes = 0;
 
-	if (h)
+	while (h)
 	{
-		while (h)
-		{
-			printf("%d\n", h->n);
-			h = h->next;
-			number_of_nodes++;
-		}
-		return (number_of_nodes);
+		printf("%d\n", h->n);
+		h = h->next;
+		number_of_nodes++;
 	}
-	return (0);
+	return (number_of_nodes);
 }
