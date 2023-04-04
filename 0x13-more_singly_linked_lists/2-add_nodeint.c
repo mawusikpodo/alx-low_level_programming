cac2631402a1fa998c76beda5fa293cab64dabd3
Robert Mawusi Kpodo <mawusikpodo@yahoo.com>
2023-04-04 13:42:46 -0700

    Task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -15,8 +15,13 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	new_node->n = n;
 
 	if (!new_node)
+	{
 		return (NULL);
-	new_node->next = *head;
-	*head = new_node;
-	return (*head);
+	}
+	else
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (*head);
+	}
 }
