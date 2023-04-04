64aa2fa48cb6ebdae3e2b602c1c6c04965361439
Robert Mawusi Kpodo <mawusikpodo@yahoo.com>
2023-04-04 14:01:52 -0700

    Task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,7 +12,6 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *new_node;
 
 	new_node = malloc(sizeof(listint_t));
-	new_node->n = n;
 
 	if (new_node == NULL)
 	{
@@ -20,6 +19,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	}
 	else
 	{
+		new_node->n = n;
 		new_node->next = *head;
 		*head = new_node;
 	}
